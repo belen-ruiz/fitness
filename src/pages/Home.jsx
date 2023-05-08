e618010fb250c2568c069ac28dfc5ba575b005eb
belen ruiz <beelen.ruiz91@gmail.com>
2023-05-08 17:16:13 -0300

    terminado base

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Footer } from '../components/common/footer/Footer'
+import { Header } from '../components/common/header/Header'
+import { Banner } from '../components/layout/banner/Banner'
 
 export const Home = () => {
   return (
     <>
     <div>Home</div>
+    <Header />
+    <Banner />
+    <Footer />
     </>
   )
 }
