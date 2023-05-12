16c19492a58e8d8991931cfee08e2c4276c3401c
belen ruiz <beelen.ruiz91@gmail.com>
2023-05-12 19:12:51 -0300

    ok

--- src/components/ExerciseDetail.jsx
+++ src/components/ExerciseDetail.jsx
@@ -3,16 +3,13 @@ import { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { fetchData, exerciseOp } from '../utils/fetchData';
 import { Exercise } from './Exercise';
+import { Publi } from './Publi';
 import { SimilarExercises } from './SimilarExercises';
 
 export const ExerciseDetail = () => {
 
-    //const [exerciseDetail, setExerciseDetail] = useState({})
     const [subcat, setSubcat] = useState({})
     const [subcategoria, setSubcategoria] = useState([])
-
-    const [isOpen, setIsOpen] = useState(false)
-
 
     const { id } = useParams();
 
@@ -30,20 +27,20 @@ export const ExerciseDetail = () => {
     }, [id])
 
     const subCatD = subcat.children_categories
+    console.log(subCatD)
     
   return (
     <>
       <h3>{subcat.name}</h3>
+
       {subcat.picture && <img src={subcat.picture} alt={subcat.name}/>}
+
 
       {subCatD && 
         subCatD.map((sub)=>(
-        <Exercise key={sub.id} subcat={subcat}/>))}
-
-        {/* <SimilarExercises 
-        similarMuscle={similarMuscle}
-        similarEquipment={similarEquipment}/> */}
-
+          <Exercise key={sub.id} subcat={subcat}/>))}
+    
+      {subCatD <= 0 && <Publi subcat={subcat}/>}
     </>
   )
 }
---
+++ src/components/Publi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Publi = ({subcat}) => {
+
+    console.log(subcat)
+  return (
+    <div>Publi</div>
+  )
+}
