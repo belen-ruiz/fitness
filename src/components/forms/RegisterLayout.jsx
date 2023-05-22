9c8d5c0799688ef72bd900a5b27ef5e99d39e1b9
belen ruiz <beelen.ruiz91@gmail.com>
2023-05-22 19:55:14 -0300

    acicando ok

--- src/components/forms/RegisterLayout.jsx
+++ src/components/forms/RegisterLayout.jsx
@@ -4,75 +4,91 @@ import { GoogleLoginButton } from "react-social-login-buttons";
 import { LinkedInLoginButton } from "react-social-login-buttons";
 import { inputsSignUp } from "../constantes/inputs";
 
+const RegexErrors = () => {
+    return (
+        // (errors.name && <p>{errors.name}</p>) ||
+        // (errors.email && <p>{errors.email}</p>)
+        <></>
+    );
+};
 
+const InitialRegister = ({ handleClickUp }) => {
+    return (
+        <>
+            <div>
+                <div>REGISTER</div>
+                <div href="#">Crea tu cuenta</div>
+                <button onClick={handleClickUp}>Sigin up</button>
+            </div>
+        </>
+    );
+};
 
-const RegisterLayout = ({
+const RegisterForm = ({ handleChange, handleBlur, handleSubmit }) => {
+    return (
+        <div>
+            <div className="title">Join Us!</div>
+
+            <div className="subtitle">Create your new account</div>
+
+            <form onSubmit={handleSubmit}>
+                {inputsSignUp &&
+                    inputsSignUp.map((input) => (
+                        <div key={input.name}>
+                            <label htmlFor={input.name}>{input.label}</label>
+                            <input
+                                type={input.type}
+                                name={input.name}
+                                placeholder={input.placeholder}
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                required={input.required}
+                            />
+                        </div>
+                    ))}
+
+                <RegexErrors />
+
+                <input type="submit" value="Enviar" onClick={handleSubmit} />
+            </form>
+
+            <div>----or-----</div>
+
+            <div>
+                <div>
+                    <GoogleLoginButton />
+                </div>
+                <div>
+                    
+                    <LinkedInLoginButton />
+                </div>
+                <div>
+                    
+                    <GithubLoginButton />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export const RegisterLayout = ({
     signUp,
-    value,
-    errors,
     handleChange,
     handleBlur,
     handleSubmit,
     handleClickUp,
 }) => {
-
     return (
         <>
-        {signUp ?
-        <div className="">
-            
-                <div className="title">Join Us!</div>
-            
-                <div className="subtitle">Create your new account</div>
-            
-                <form onSubmit={handleSubmit}>
-                    {inputsSignUp &&
-                        inputsSignUp.map((input) => (
-                            <div key={input.name}>
-                                <label htmlFor={input.name}>{input.label}</label>
-                                <input
-                                    type={input.type}
-                                    name={input.name}
-                                    placeholder={input.placeholder}
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    required={input.required}
-                                    value={value}
-                                />
-                            </div>
-                        ))}
-            
-                    {/* {(errors.name && <p>{errors.name}</p>) ||
-                        (errors.email && <p>{errors.email}</p>)} */}
-            
-            
-                    <input type="submit" value="Enviar" onClick={handleSubmit} />
-                </form>
-            
-                <div>----or-----</div>
-            
-                <div>
-                    <div> <GoogleLoginButton /> </div>
-                    <div> <LinkedInLoginButton /> </div>
-                    <div> <GithubLoginButton /> </div>
-                </div>
-        </div>
-        : 
-        <InitialRegister handleClickUp={handleClickUp}/>}
+            {signUp ? (
+                <RegisterForm
+                    handleChange={handleChange}
+                    handleBlur={handleBlur}
+                    handleSubmit={handleSubmit}
+                />
+            ) : (
+                <InitialRegister handleClickUp={handleClickUp} />
+            )}
         </>
     );
 };
-
-const InitialRegister = ({ handleClickUp }) => {
-    return(
-    <>
-        <div className="">
-            <div>REGISTER</div>
-            <div href="#">Crea tu cuenta</div>
-            <button onClick={handleClickUp}>Sigin up</button>
-        </div>
-    </>
-    )
-}
-
-export {RegisterLayout}
--- src/components/forms/SignInLayout.jsx
+++ src/components/forms/SignInLayout.jsx
@@ -1,61 +1,66 @@
 import React from "react";
 import { inputsSignIn } from "../constantes/inputs";
 
+const InitialSignIn = ({ handleClickIn }) => {
+    return (
+        <div className="">
+            <div>SIGN IN</div>
+            <div href="#">Si ya tienes una cuenta</div>
+            <button onClick={handleClickIn}>Sigin In</button>
+        </div>
+    );
+};
 
-const SignInLayout = ({
-  signIn,
-  value,
-  errors,
-  handleChange,
-  handleBlur,
-  handleSubmit,
-  handleClickIn
+const SignInForm = ({ handleChange, handleBlur, handleSubmit }) => {
+    return (
+        <div>
+            <div className="title">Welcome back!</div>
+            <div className="subtitle">Complete with your account details</div>
+            <form onSubmit={handleSubmit}>
+                {inputsSignIn &&
+                    inputsSignIn.map((input) => (
+                        <div key={input.name}>
+                            <label htmlFor={input.name}>{input.label}</label>
+                            <input
+                                type={input.type}
+                                name={input.name}
+                                placeholder={input.placeholder}
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                required={input.required}
+                            />
+                        </div>
+                    ))}
+
+                <div href="#">Forgot your password?</div>
+
+                <input type="submit" value="Enviar" onClick={handleSubmit} />
+            </form>
+        </div>
+    );
+};
+
+export const SignInLayout = ({
+    signIn,
+    value,
+    errors,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    handleClickIn,
 }) => {
     return (
         <>
-            { signIn ? 
-            <div>
-                <div className="title">Welcome back!</div>
-                <div className="subtitle">Complete with your account details</div>
-                <form onSubmit={handleSubmit}>
-                    {inputsSignIn &&
-                        inputsSignIn.map((input) => (
-                            <div key={input.name}>
-                                <label htmlFor={input.name}>{input.label}</label>
-                                <input
-                                    type={input.type}
-                                    name={input.name}
-                                    placeholder={input.placeholder}
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    required={input.required}
-                                    value={value}
-                                />
-                            </div>
-                        ))}
-            
-                    {/* {(errors.name && <p>{errors.name}</p>) ||
-                        (errors.email && <p>{errors.email}</p>)} */}
-            
-                    <div href="#">Forgot your password?</div>
-            
-                    <input type="submit" value="Enviar" onClick={handleSubmit} />
-                </form>
-            </div>
-            :
-            
-            <InitialSignIn handleClickIn={handleClickIn} />}
+            {signIn ? (
+                <SignInForm
+                    handleChange={handleChange}
+                    handleBlur={handleBlur}
+                    handleSubmit={handleSubmit}
+                />
+            ) : (
+                <InitialSignIn handleClickIn={handleClickIn} />
+            )}
         </>
     );
 };
 
-const InitialSignIn = ({ handleClickIn }) => {
-    return(
-    <div className="">
-    <div>SIGN IN</div>
-    <div href="#">Si ya tienes una cuenta</div>
-    <button onClick={handleClickIn}>Sigin In</button>
-    </div>)
-}
-
-export {SignInLayout}
